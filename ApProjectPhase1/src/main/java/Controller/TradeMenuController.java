681e63008f12a5894ac9de8ba5d1dde4676844c1
tarakemeh8 <tarakemeh8@gmail.com>
2023-04-25 20:32:45 +0330

    edit TradeMenuController

--- ApProjectPhase1/src/main/java/Controller/TradeMenuController.java
+++ ApProjectPhase1/src/main/java/Controller/TradeMenuController.java
@@ -1,11 +1,26 @@
 package Controller;
 
+import View.LoginMenu.LoginMenuCommands;
+import View.TradeMenu.TradeMenuCommands;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class TradeMenuController {
     public static String trade(String content, Scanner scanner) {
-
+        Matcher matcher;
+        //TODO:check valid resource type
+        int price;
+        if (!(matcher = TradeMenuCommands.getMatcher(content, TradeMenuCommands.RESOURCE_PRICE_FIELD)).find())
+            return "price field is empty";
+        else if (matcher.results().count() > 1)
+            return "invalid command";
+        else {
+            price = Integer.parseInt(matcher.group("price"));
+            if (price < 0) {
+                return null; //TODO
+            }
+        }
     }
 
     public void requestTrade(Matcher matcher) {
